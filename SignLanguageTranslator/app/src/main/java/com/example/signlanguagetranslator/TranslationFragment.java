6565d3aa030c5bbbab2ca972f4757c3c57c5780d
YJH2036 <52653285+YJH2036@users.noreply.github.com>
2020-02-20 23:54:13 +0900

    200220 YJH - 1

--- SignLanguageTranslator/app/src/main/java/com/example/signlanguagetranslator/TranslationFragment.java
+++ SignLanguageTranslator/app/src/main/java/com/example/signlanguagetranslator/TranslationFragment.java
@@ -21,68 +21,13 @@ import java.net.Socket;
 
 public class TranslationFragment extends Fragment {
 
-    private Socket clientSocket;
-    private BufferedReader socketIn;
-    private PrintWriter socketOut;
-    private int port = 9999;
-    private final String ip = "223.195.222.138";
-    private MyHandler myHandler;
-    private MyThread myThread;
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-        StrictMode.setThreadPolicy(policy);
 
-        try {
-            clientSocket = new Socket(ip, port);
-            socketIn = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            socketOut = new PrintWriter(clientSocket.getOutputStream(), true);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        myHandler = new MyHandler();
-        myThread = new MyThread();
-        myThread.start();
-
-        //btn = (Button) findViewById(R.id.btn);
-        //tv = (TextView) findViewById(R.id.tv);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                socketOut.println(123);
-            }
-        });
-
-        class MyThread extends Thread {
-            @Override
-            public void run() {
-                while (true) {
-                    try {
-                        // InputStream의 값을 읽어와서 data에 저장
-                        String data = socketIn.readLine();
-                        // Message 객체를 생성, 핸들러에 정보를 보낼 땐 이 메세지 객체를 이용
-                        Message msg = myHandler.obtainMessage();
-                        msg.obj = data;
-                        myHandler.sendMessage(msg);
-                    }
-                    catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
-
-        class MyHandler extends Handler {
-            @Override
-            public void handleMessage(Message msg) {
-                //tv.setText(msg.obj.toString());
-            }
-        }
 
 
 
