f69166df4c2ec35efb6b319c9b67e1dbd25e9bfa
YJH2036 <52653285+YJH2036@users.noreply.github.com>
2020-03-08 23:58:30 +0900

    200308 YJH - 1

--- SignLanguageTranslator/app/src/main/java/com/example/signlanguagetranslator/ListFragment.java
+++ SignLanguageTranslator/app/src/main/java/com/example/signlanguagetranslator/ListFragment.java
@@ -7,12 +7,22 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class ListFragment extends Fragment {
+public class ListFragment extends Fragment  {
+    ListView listView;
+    ArrayList<String> item;
+    ArrayAdapter<String> mAdapter;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
 
     }
 
@@ -20,6 +30,13 @@ public class ListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
+        listView = (ListView)view.findViewById(R.id.listview);
+
+        String[] items = new String[]{"item1", "item2", "item3"};
+        
+
+        return view;
+
     }
 }
