720efc290b9b1af4b7d29aead6a5ac21cb683c0f
josephna97 <49613651+josephna97@users.noreply.github.com>
2020-03-24 22:25:14 +0900

    200324 - KJS

--- Ing/bluetoothconnect/app/src/main/java/com/example/bluetoothconnect/SettingActivity.java
+++ Ing/bluetoothconnect/app/src/main/java/com/example/bluetoothconnect/SettingActivity.java
@@ -2,8 +2,16 @@ package com.example.bluetoothconnect;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioButton;
 
 public class SettingActivity extends AppCompatActivity {
+
+    RadioButton button_050;
+    RadioButton button_075;
+    RadioButton button_100;
+    RadioButton button_125;
+    RadioButton button_150;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
