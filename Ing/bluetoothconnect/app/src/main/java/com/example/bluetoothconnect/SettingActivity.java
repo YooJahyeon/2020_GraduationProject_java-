c10ab8655e05f266226ab414c1eda2c53b094118
josephna97 <49613651+josephna97@users.noreply.github.com>
2020-03-27 22:48:55 +0900

    200327 - KJS

--- Ing/bluetoothconnect/app/src/main/java/com/example/bluetoothconnect/SettingActivity.java
+++ Ing/bluetoothconnect/app/src/main/java/com/example/bluetoothconnect/SettingActivity.java
@@ -2,7 +2,6 @@ package com.example.bluetoothconnect;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 import android.widget.RadioButton;
 
